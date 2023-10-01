243cadfb310ec93b2c53bdcd0520c4cee9652fa6
s-rader <rader06@gmail.com>
2023-10-01 08:09:27 -0700

    BIG-144 Responsive Website

--- my-frontend/src/pages/about.js
+++ my-frontend/src/pages/about.js
@@ -7,58 +7,34 @@ import '../App.css';
 
 					
 const About = () => {
-	const containerStyle = {
-		position: 'relative',
-		minHeight: '100vh',
-		overflow: 'hidden',
-		}
-	  
-	  
-		const deliveryStyle = {
-		  position: 'absolute',
-		  top:0,
-		  left: 0,
-		  height: '20%', // Make the image span the full viewport height
-		  width: 'auto', // Maintain the aspect ratio
-		  zIndex: 1,
-		  //   maxWidth: '100%',
-		}
-
-		const pickUpStyle = {
-			position: 'absolute',
-			top: '25%',
-			left: '3%',
-			height: '10%', // Make the image span the full viewport height
-			width: 'auto', // Maintain the aspect ratio
-			zIndex: 1,
-		  }
-		
-	  
-		const grayElipseStyle = {
-			position: 'absolute',
-			bottom: -200,
-			right: -200,
-			width: '120%',
-			height: 'auto', /* Maintain the aspect ratio */
-			zIndex: 0,
-		  }
 
   return (
-    <div style={containerStyle}>
-		<img src={grayElipse} alt='pizza'  style={grayElipseStyle}/>
-        <img src={delivery} alt='pizza' style={deliveryStyle} />
-        <img src={pickUp} alt='pizza' style={pickUpStyle} />
-		<p className='addressStyle'>1852 Country Club Blvd<br />Stockton CA 95212</p>
-		<h1  className="aboutHeader">
-			WHO ARE WE?
-		</h1>
-		<p className='aboutText'>
-			SAM'S PIZZA && MORE.......... is a local fast food restaurant that has been serving the community for about 20 years. 
-			We offer a variety of food choices such as pizza with many toppings, fried foods, juicy drinks, and more. <br />
-			We're dedicated to serve everyone with the best quality and reasonable price.
-		</p>
+	<div>
+		<div className="aboutContainerStyle">
+			<div className='aboutColumnPictures'>
+				<img src={delivery} alt='Delivery' className="deliveryStyle" style={{ zIndex: 1 }} />
+				<br />
+				<img src={pickUp} alt='Pick Up' className="pickUpStyle" style={{ zIndex: 1 }} />
+			</div>
+			<div className='aboutColumnCenter'>
+				<h1  className="aboutHeader">
+					WHO ARE WE?
+				</h1>
+				<p className="aboutText">
+					SAM'S PIZZA && MORE.......... is a local fast food restaurant that has been serving the community for about 20 years. 
+					We offer a variety of food choices such as pizza with many toppings, fried foods, juicy drinks, and more. <br />
+					We're dedicated to serve everyone with the best quality and reasonable price.
+				</p>
+			</div>
+			<div className='aboutColumnAddress'>
+				<p className="addressStyle">1852 Country Club Blvd<br />Stockton CA 95212</p>
+			</div>
+		</div>
+		<div className='grayElipseContainer'>
+			<img src={grayElipse} alt='GrayElipse' className="grayElipseStyle"/>
+	 	</div>
 	</div>
-		);
+	);
 };
 
 export default About;
--- my-frontend/src/pages/mainmenu.js
+++ my-frontend/src/pages/mainmenu.js
@@ -15,60 +15,60 @@ import hotwings_webp from '../images/Other/hotwingsCartoon.webp';
 import taco_webp from '../images/Other/tacoCartoon.webp';
 import icecream_webp from '../images/Other/icecreamCartoon.webp';
 
-const styles = {
-  container: {
-    margin: '5% 10%',
-    paddingTop: '20px',
-    paddingBottom: '20px',
-    paddingLeft: 'min(20px, 0.1vw)',
-    paddingRight: 'min(20px, 0.1vw)',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    minHeight: '300px',
-    minWidth: '300px',
-  },
-  header: {
-    display: 'flex',
-    width: '100%',
-    fontSize: 'min(40px, 8vw)',
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'var(--clr-txt-dark)'
-  },
-  downArrow: {
-    marginLeft: 'max(20px, 2vw)',
-    marginRight: 'max(20px, 2vw)',
-    height: 'min(60px, 12vw)',
-  },
-  submenuGrid: {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
-    gridColumnGap: 'min(40px, 4vw)',
-    gridRowGap: 'min(30px, 1.5vh)',
-    marginTop: '50px',
-    minHeight: '300px',
-    minWidth: '300px',
-  },
-  pictureGrid: {
-    display: 'grid',
-    width: '100%',
-    maxWidth: '100%',
-    gridTemplateColumns: '1fr 1fr 1fr',
-    gap: '10%',
-    alignItems: 'center',
-    alignText: 'center',
-    marginTop: '50px',
-  }, 
-  picture: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%',
-  },
-};
+// const styles = {
+//   container: {
+//     margin: '5% 10%',
+//     paddingTop: '20px',
+//     paddingBottom: '20px',
+//     paddingLeft: 'min(20px, 0.1vw)',
+//     paddingRight: 'min(20px, 0.1vw)',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     height: '100%',
+//     minHeight: '300px',
+//     minWidth: '300px',
+//   },
+//   header: {
+//     display: 'flex',
+//     width: '100%',
+//     fontSize: 'min(40px, 8vw)',
+//     textAlign: 'center',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     color: 'var(--clr-txt-dark)'
+//   },
+//   downArrow: {
+//     marginLeft: 'max(20px, 2vw)',
+//     marginRight: 'max(20px, 2vw)',
+//     height: 'min(60px, 12vw)',
+//   },
+//   submenuGrid: {
+//     display: 'grid',
+//     gridTemplateColumns: '1fr 1fr',
+//     gridColumnGap: 'min(40px, 4vw)',
+//     gridRowGap: 'min(30px, 1.5vh)',
+//     marginTop: '50px',
+//     minHeight: '300px',
+//     minWidth: '300px',
+//   },
+//   pictureGrid: {
+//     display: 'grid',
+//     width: '100%',
+//     maxWidth: '100%',
+//     gridTemplateColumns: '1fr 1fr 1fr',
+//     gap: '10%',
+//     alignItems: 'center',
+//     alignText: 'center',
+//     marginTop: '50px',
+//   }, 
+//   picture: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     height: '100%',
+//     width: '100%',
+//   },
+// };
 
 const SubmenuButton = styled.button`
 font-size: min(35px, 5vw);
@@ -89,34 +89,34 @@ const MainMenu = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={styles.container}>
-      <div style={styles.header}>
-        <img src={downArrow} style={styles.downArrow} alt='downArrow-image' />
+    <div className="mainMenuContainer">
+      <div className="mainMenuHeader">
+        <img src={downArrow} className="mainMenuDownArrow" alt='downArrow-image' />
         <span>CHECK OUT OUR MENU</span>
-        <img src={downArrow} style={styles.downArrow} alt='downArrow-image' />
+        <img src={downArrow} className="mainMenuDownArrow" alt='downArrow-image' />
       </div>
-      <div style={styles.submenuGrid}>
+      <div className="mainMenuSubmenuGrid" >
         <SubmenuButton onClick={() => navigate('/lunchMenu')}>LUNCH & DINNER</SubmenuButton>
         <SubmenuButton onClick={() => navigate('/drink')}>DRINK SPECIALTIES</SubmenuButton>
         <SubmenuButton onClick={() => navigate('/pizzaSpecial')}>PIZZA SPECIALTIES</SubmenuButton>
         <SubmenuButton onClick={() => navigate('/comboSpecial')}>COMBO SPECIALTIES</SubmenuButton>
         <SubmenuButton onClick={() => navigate('/specialDeals')}>SPECIAL DEALS</SubmenuButton>
       </div>
-      <div style={styles.pictureGrid}>
+      <div className="mainMenuPictureGrid">
         <picture>
           <source type="image/webp" srcSet={hotwings_webp} />
           <source type="image/png" srcSet={hotwings} />
-          <img src={hotwings} style={styles.picture} alt='hotwings-image' />
+          <img src={hotwings} className="mainMenuPicture" alt='hotwings-image' />
         </picture>
         <picture>
           <source type="image/webp" srcSet={taco_webp} />
           <source type="image/png" srcSet={taco} />
-          <img src={taco} style={styles.picture} alt='taco-image' />
+          <img src={taco} className="mainMenuPicture" alt='taco-image' />
         </picture>
         <picture>
           <source type="image/webp" srcSet={icecream_webp} />
           <source type="image/png" srcSet={icecream} />
-          <img src={icecream} style={styles.picture} alt='icecream-image' />
+          <img src={icecream} className="mainMenuPicture" alt='icecream-image' />
         </picture>
       </div>
     </div>
