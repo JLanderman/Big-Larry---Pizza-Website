88ec3412c01a31c6bf883e6f528c23961d906e9d
s-rader <rader06@gmail.com>
2023-10-15 21:14:37 -0700

    BIG-150: Update itemDAO.js. to use 'allItems'

--- my-backend/dao/itemDAO.js
+++ my-backend/dao/itemDAO.js
@@ -1,37 +1,17 @@
-let item;
-let cart;
-let lunch;
 let allItems;
-
 
 export default class ItemDao {
   /*
    *   database is put into the variable item for querying
    */
   static async injectDB(conn) {
-    if (item) {
-      return;
-    }
-    item = await conn.db(process.env.ITEM_NS).collection("menuItems");
-
-    if (cart) {
-      return;
-    }
-    cart = await conn.db(process.env.ITEM_NS).collection("cartItems");
-
- 
-    if(lunch)
-    {
-      return;
-    }
-    lunch = await conn.db(process.env.ITEM_NS).collection("lunchItems");
-
     if (allItems) {
       return;
     }
     allItems = await conn.db(process.env.ITEM_NS).collection("allMenuItems");
   }
 
+  // getItem() gets all items.
   static async getItem() {
     let query;
 
@@ -53,37 +33,15 @@ export default class ItemDao {
     return { itemList, totalNumItem };
   }
 
-  static async getCart() {
-    let query;
-
-    let cursor;
-
-    try {
-      cursor = await cart.find(query);
-    } catch (e) {
-      console.error(
-        `Unable to issue the getCart find command in cartDAO.js, ${e}`
-      );
-      return { cartList: [], totalNumItem: 0 };
-    }
-
-    const displayCursor = cursor.limit(100).skip(0);
-    const cartList = await displayCursor.toArray();
-    const totalNumItem = await cart.countDocuments(query);
-
-    return { cartList, totalNumItem };
-  }
-
-
   static async getLunch()
   {
     let query;
-    
+    query = {itemCategory: "lunch/Dinner"};
     let cursor;
 
     try
     {
-      cursor = await lunch.find(query);
+      cursor = await allItems.find(query);
     }catch (e)
     {
       console.error(
@@ -93,16 +51,12 @@ export default class ItemDao {
     }
 
     const displayCursor = cursor.limit(100).skip(0);
-    const luchList = await displayCursor.toArray();
-    const totalNumItem = await lunch.countDocuments(query);
+    const lunchList = await displayCursor.toArray();
+    const totalNumItem = await allItems.countDocuments(query);
 
-    return {luchList, totalNumItem};
+    return {lunchList, totalNumItem};
 
   }
-
-
-
-
 
   //overloading this function with a variation for getting specific MongoDB "_id"s
   static async getItem(DesiredObjectId) {
@@ -110,7 +64,7 @@ export default class ItemDao {
     let cursor;
 
     try {
-      cursor = await allItems.find(DesiredObjectId);
+      cursor = await allItems.find(DesiredObjectId); // Shouldn't this be finding the query instead of DesiredObjectID directly?
     } catch (e) {
       console.error(
         `Unable to issue the getItem(id) find command in itemDAO.js, ${e}`
@@ -120,54 +74,40 @@ export default class ItemDao {
 
     const displayCursor = cursor.limit(100).skip(0);
     const itemList = await displayCursor.toArray();
-    const totalNumItem = await item.countDocuments(query);
+    const totalNumItem = await allItems.countDocuments(query);
 
     return { itemList, totalNumItem };
   }
 
-  static async getCart(DesiredObjectId) {
-    let query;
-    let cursor;
 
-    try {
-      cursor = await cart.find(DesiredObjectId);
-    } catch (e) {
-      console.error(
-        `Unable to issue the getCart find command in cartDAO.js, ${e}`
-      );
-      return { cartList: [], totalNumItem: 0 };
-    }
+  // Removed: The overloaded function for getting lunch items by ID.
+  // This breaks the normal lunch display code.
+  // If you need a specific item by ID, consider using the overloaded get item function above since they're all in the same collection.
+  // You can probably remove this if it doesn't break anything.
 
-    const displayCursor = cursor.limit(100).skip(0);
-    const cartList = await displayCursor.toArray();
-    const totalNumItem = await cart.countDocuments(query);
+  // static async getLunch(DesiredObjectId)
+  // {
+    // let query;
+    // let cursor;
 
-    return { cartList, totalNumItem };
-  }
-
-  static async getLunch(DesiredObjectId)
-  {
-    let query;
-    let cursor;
-
-    try{
-      cursor = await lunch.find(DesiredObjectId);
-      console.log('check2',lunch)
-    }catch(e)
-    {
-      console.error(
-        `Unable to issue the getLunchItem find command in lunchDAO.js, ${e}`
-      );
-      return {lunchList: [] , totalNumItem: 0}
-    }
-    console.log('check1',lunch)
-    const displayCursor = cursor.limit(100).skip(0);
-    const lunchList = await displayCursor.toArray();
-    const totalNumItem = await lunch.countDocuments(query);
-    console.log('check3',lunchList)
-    console.log('check4',totalNumItem)
-    return { lunchList, totalNumItem};
-  }
+    // try{
+    //   cursor = await allItems.find(DesiredObjectId);
+    //   console.log('check2',allItems)
+    // }catch(e)
+    // {
+    //   console.error(
+    //     `Unable to issue the getLunchItem find command in lunchDAO.js, ${e}`
+    //   );
+    //   return {lunchList: [] , totalNumItem: 0}
+    // }
+    // console.log('check1',allItems)
+    // const displayCursor = cursor.limit(100).skip(0);
+    // const lunchList = await displayCursor.toArray();
+    // const totalNumItem = await allItems.countDocuments(query);
+    // console.log('check3',lunchList)
+    // console.log('check4',totalNumItem)
+    // return { lunchList, totalNumItem};
+  // }
 
   static async getPizzaSpecial() {
     let query;
