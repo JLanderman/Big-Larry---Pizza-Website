dcab32b790480693ac09dee00ca3c1f221f4ccca
corvax110 <jefftaylor110@hotmail.com>
2023-10-29 18:14:58 -0700

    BIG-174 Redo Customized Pizza options page

--- my-frontend/src/pages/pizza_customize.js
+++ my-frontend/src/pages/pizza_customize.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import styles from "./pizza_customize.css";
-import PizzaCombinationImage from "../images/Pizza Specialties/Pizza Combination.png"; 
+import PizzaCombinationImage from "../images/Pizza Specialties/Pizza Combination.png";
 
 
 import { useState, useEffect } from "react";
@@ -15,26 +15,26 @@ const moreStyles = {
     maxWidth: '150px',
     minHeight: '',
     height: '25%',
-    paddingLeft:'0%',
-    paddingRight:'',
+    paddingLeft: '0%',
+    paddingRight: '',
     justifyContent: 'center',
-    fontSize:'120%'
+    fontSize: '120%'
   },
-  button:{
-		boxSizing:'border-box', 
-		backgroundColor: "rgba(52, 52, 52, 0.08)",
-		paddingTop:'10px', 
-		paddingBottom:'10px',
-    paddingRight:'10%',
-    paddingLeft:'25%',
-		color: "black",
-		justifyContent:'center',
-		border:'0px solid black'
-	},
+  button: {
+    boxSizing: 'border-box',
+    backgroundColor: "rgba(52, 52, 52, 0.08)",
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    paddingRight: '10%',
+    paddingLeft: '25%',
+    color: "black",
+    justifyContent: 'center',
+    border: '0px solid black'
+  },
 }
 
 const Pizza_customize = () => {
-  
+
   const priceCheesePersonal = '4.99'
   const priceCheeseSmall = '6.50'
   const priceCheeseMedium = '7.99'
@@ -65,13 +65,20 @@ const Pizza_customize = () => {
   const priceComboLarge = '18.99'
   const priceComboXLarge = '22.99'
 
+  const priceExtraPersonal = '1.00'
+  const priceExtraSmall = '1.50'
+  const priceExtraMedium = '2.00'
+  const priceExtraLarge = '2.50'
+  const priceExtraXLarge = '3.00'
+
   //2d array is necessary for determining price without a bunch of if/else statements
   const priceChart = [ // [row][column]=[topping][size]
     [priceCheesePersonal, priceCheeseSmall, priceCheeseMedium, priceCheeseLarge, priceCheeseXLarge],
     [priceSinglePersonal, priceSingleSmall, priceSingleMedium, priceSingleLarge, priceSingleXLarge],
-    [priceTwoPersonal   , priceTwoSmall   , priceTwoMedium   , priceTwoLarge   , priceTwoXLarge],
-    [priceThreePersonal , priceThreeSmall , priceThreeMedium , priceThreeLarge , priceThreeXLarge],
-    [priceComboPersonal , priceComboSmall , priceComboMedium , priceComboLarge , priceComboXLarge]
+    [priceTwoPersonal, priceTwoSmall, priceTwoMedium, priceTwoLarge, priceTwoXLarge],
+    [priceThreePersonal, priceThreeSmall, priceThreeMedium, priceThreeLarge, priceThreeXLarge],
+    [priceComboPersonal, priceComboSmall, priceComboMedium, priceComboLarge, priceComboXLarge],
+    [priceExtraPersonal, priceExtraSmall, priceExtraMedium, priceExtraLarge, priceExtraXLarge]
   ]
 
 
@@ -83,26 +90,26 @@ const Pizza_customize = () => {
 
   const toppingList = [
     "Cheese",            //why is cheese a topping?
-    "Pepperoni", 
-    "Salami", 
-    "Ground Beef", 
-    "Sausage", 
-    "Linguica", 
-    "Chicken", 
-    "Ham", 
-    "Onion", 
+    "Pepperoni",
+    "Salami",
+    "Ground Beef",
+    "Sausage",
+    "Linguica",
+    "Chicken",
+    "Ham",
+    "Onion",
     "Bell pepper",
-    "Cilantro", 
-    "Pineapple", 
-    "Tomato", 
-    "Artichoke", 
-    "Pepperoncini", 
+    "Cilantro",
+    "Pineapple",
+    "Tomato",
+    "Artichoke",
+    "Pepperoncini",
     "Anchovies",
   ];
   const [selectedSize, setSelectedSize] = useState(0); //default size is personal
 
   const [selectedTopping, setSelectedTopping] = useState([]);
-  
+
   const [price, setPrice] = useState(priceSelect(selectedSize, selectedTopping.length)) //initializes price properly
 
   const handleSizeClick = (index) => {
@@ -123,8 +130,8 @@ const Pizza_customize = () => {
     }
   }; // This block of code is for selecting >1 possible toppings
 
-  function priceSelect(size, toppings){
-    if (toppings > 3){
+  function priceSelect(size, toppings) {
+    if (toppings > 3) {
       return priceChart[4][size];
     } else {
       return priceChart[toppings][size];
@@ -134,67 +141,99 @@ const Pizza_customize = () => {
 
   return (
     <div>
-      <div className = "titleCustom">
-        <div> 
-          <div data-testid="pizzaCustom"><h1>Choose your size</h1> </div>
-        </div>
-      </div>
-
-      <div className="sizeButtonsCustom">
-        {sizeList.map((size, index) => (
-
-          <button
-            key={index}
-            className={`pizzaSizeButtonCustom ${
-              selectedSize === index ? "selected" : ""
-            }`}
-            onClick={() => handleSizeClick(index)}
-          >
-            {size}
-          </button>
-
-        ))}
-      </div>
-
-      <div className = "titleCustom">
+      <div className="titleCustom">
         <div>
-          <div><h1>Choose your toppings</h1> </div>
+          <div><h1 style={{color:'red'}}>Topping Options</h1> </div>
         </div>
       </div>
-
-      <div className="toppingButtonsCustom">
+      <div className="toppingButtonsCustom" style={{maxWidth: '80%',minWidth:'610px'}}>
         {toppingList.map((size, index) => (
-
-          <button
-            key={index}
-            className={`pizzaToppingButtonCustom ${
-              selectedTopping.includes(index) ? "selected" : ""
-            }`}
-            onClick={() => handleToppingClick(index)}
-          >
+          <span className="pizzaToppingButtonCustom">
             {size}
-          </button>
-
+          </span>
         ))}
       </div>
 
-      <div className = "myContainerCustom">
-        <div className = "pictureCustom">
-         <img src={PizzaCombinationImage} alt="Pizza CombinationCustom" />
+      <div className="titleCustom">
+        <div>
+          <div data-testid="pizzaCustom"><h1 style={{color:'red'}}>Your Choice of Pizza</h1> </div>
         </div>
-
-        <div className = "descriptionCustom" style={moreStyles.innerContainer}>
-          <div className = "square rounded-pill" style={moreStyles.button}>
-            <h4 style={{fontSize:'150%'}}>
-              Price:
-            </h4>
-            <h5 style={{fontSize:'150%'}}>
-              ${price}
-            </h5>
-          </div>
-        </div>
-
       </div>
+      <div style={{ border: '0px solid', paddingLeft: '20%' }}>
+        <table style={{ border: '0px solid', width:'85%', maxWidth: '950px'}}>
+          <tr style={{ borderBottom: '1px solid', padding: '5px', fontSize: '120%', }}>
+            <th style={{ borderRight: '1px solid', minWidth: '100px' }}>
+              <div className="diagonalHeader" style={{display: 'grid', gridTemplateAreas: "'. a' 'b .'"}}>
+                <span style={{gridArea:'a', color: 'red', fontSize: '90%'}}>
+                  Size
+                </span>
+                <span style={{gridArea:'b', color: 'red', fontSize: '90%'}}>
+                  Topping
+                </span>
+              </div>
+            </th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingLeft: '10px' }}>Personal</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>Small</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>Medium</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>Large</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>X-Large</th>
+          </tr>
+          <tr style={{ padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', minHeight: '1000px', minWidth: '100px' }}>Cheese</th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px',paddingLeft: '10px' }}>$ {priceChart[0][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[0][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[0][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[0][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[0][4]}</th>
+          </tr>
+          <tr style={{ padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', minWidth: '100px' }}>1 topping</th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px', paddingLeft: '10px' }}>$ {priceChart[1][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[1][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[1][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[1][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[1][4]}</th>
+          </tr>
+          <tr style={{ padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', minWidth: '100px' }}>2 topping</th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px', paddingLeft: '10px' }}>$ {priceChart[2][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[2][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[2][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[2][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[2][4]}</th>
+          </tr>
+          <tr style={{ padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', minWidth: '100px' }}>3 topping</th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px', paddingLeft: '10px' }}>$ {priceChart[3][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[3][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[3][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[3][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[3][4]}</th>
+          </tr>
+          <tr style={{ padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', flexWrap: 'wrap', textAlign: "left" }}>
+              4 or more toppings
+              <br></br>
+              <h5 style={{fontSize: '90%'}}>(Combo/All Meat/Veggie)</h5>
+            </th>
+
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px', paddingLeft: '10px' }}>$ {priceChart[4][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[4][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[4][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[4][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[4][4]}</th>
+          </tr>
+          <tr style={{ borderTop: '1px solid', padding: '5px', fontSize: '120%', textAlign: "left" }}>
+            <th style={{ borderRight: '1px solid', minWidth: '100px' }}>Xtra-Topping</th>
+            <th style={{ border: '0px solid', minWidth: '110px', paddingTop: '10px', paddingLeft: '10px' }}>$ {priceChart[5][0]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[5][1]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[5][2]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[5][3]}</th>
+            <th style={{ border: '0px solid', minWidth: '100px' }}>$ {priceChart[5][4]}</th>
+          </tr>
+        </table>
+      </div>
+
     </div>
   );
 };
