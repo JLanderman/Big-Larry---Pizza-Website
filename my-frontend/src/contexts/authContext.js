d551aa88dfdc20e7dc8b9b50f48268d53ecbf5c5
jkerzic <jlkerzic@gmail.com>
2023-11-04 09:26:52 -0700

    BIG-175 AuthContext now loads correctly on refresh

--- my-frontend/src/contexts/authContext.js
+++ my-frontend/src/contexts/authContext.js
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { decodeJwt } from "jose";
 
 const AuthContext = createContext({
     auth: false, // authorized admin
@@ -12,6 +14,23 @@ export const useAuth = () => useContext(AuthContext); // used by children
 const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState(false);
     const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => { // set initial auth status based on presence of token
+        const jwt = Cookies.get('x-auth-token');
+
+        if (jwt){
+            const claims = decodeJwt(jwt);
+            const exp = new Date(claims.expiration);
+            const now = new Date().getTime();
+        
+            if (now > exp){ // expired token
+                Cookies.remove('x-auth-token');
+            } else { // currently logged in
+                setLoggedIn(true);
+                if (claims.user.isAdmin && claims.user.isAdmin == true) setAuth(true);
+            }
+        };
+    });
 
     const value = { // makes values visible to children
         auth,
