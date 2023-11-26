a47f6eba8e394e5717834fac52a4c6ca417bb2cd
corvax110 <jefftaylor110@hotmail.com>
2023-11-26 15:17:55 -0800

    BIG-207 Move Login/Logout link to footer

--- my-frontend/src/components/Footer/index.js
+++ my-frontend/src/components/Footer/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { Container, Div } from "./FooterElements";
 import { NavLink } from "../Navbar/NavbarElements";
+import { useAuth } from '../../contexts/authContext';
+import Cookies from 'js-cookie';
 import { phoneNum, address } from "../../data/global";
 
 const Footer = () => {
+  const { auth, setAuth, loggedIn, setLoggedIn } = useAuth();
+  const logout = () => {
+    Cookies.remove('x-auth-token');
+    setAuth(false);
+    setLoggedIn(false);
+  };
   return (
     <Container data-testid="footer">
       <Div>
-        <span style={{color: 'var(--clr-txt-light)'}} data-testid="footerPhoneNumber">{phoneNum}</span>
+        <span style={{ color: 'var(--clr-txt-light)' }} data-testid="footerPhoneNumber">{phoneNum}</span>
         <br />
-        <span style={{color: 'var(--clr-txt-light)'}} data-testid="footerAddress">{address}</span>
+        <span style={{ color: 'var(--clr-txt-light)' }} data-testid="footerAddress">{address}</span>
       </Div>
-      <Div>
-        <NavLink to="https://www.facebook.com/bestfoodcountryclub/">
-          Facebook
-        </NavLink>
+      <Div style={{ display: 'flex', paddingRight: '1%', marginRight: '-5%' }}>
+        <Div>
+          <NavLink to="https://www.facebook.com/bestfoodcountryclub/">
+            Facebook
+          </NavLink>
+
+        </Div>
+        <Div>
+          {loggedIn
+            ? <NavLink to="/" onClick={() => { logout() }}>LOG OUT</NavLink>
+            : <NavLink to="/login">SIGN IN</NavLink>
+          }
+        </Div>
       </Div>
     </Container>
   );
