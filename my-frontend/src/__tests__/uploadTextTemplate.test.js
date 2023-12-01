8305489720708b7e2f47e05bdfdab5a740fd8ed3
jkerzic <jlkerzic@gmail.com>
2023-11-30 22:05:07 -0800

    Added tests for uploadTextTemplate.js

--- my-frontend/src/__tests__/uploadTextTemplate.test.js
+++ my-frontend/src/__tests__/uploadTextTemplate.test.js
@@ -1,14 +1,40 @@
 import { React } from "react";
-import { render, screen, cleanup, waitFor } from '@testing-library/react';
+import { render, screen, cleanup, waitFor, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import TextForm from "../pages/uploadTextTemplate";
+import UserService from "../services/UserData";
+import DataService from "../services/itemData";
+import { act } from "react-test-renderer";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Cookies from 'js-cookie';
 
-// polyfill for fetch
-import 'whatwg-fetch'
+// Mock responses from services
+jest.mock('../services/itemData');
+jest.mock('../services/UserData');
+jest.mock('js-cookie');
+const mockResponse = {
+    data: [
+        {
+            _id: '1',
+            name: 'textItem',
+            price: 1500,
+        },
+    ],
+};
+const mockResponseMultiplePrices = {
+    data: [
+        {
+            _id: '1',
+            name: 'textItem',
+            price_small: 899,
+            price_large: 999,
+        },
+    ],
+};
 
-beforeEach(async () => {
-    render(<TextForm />);
-    await waitFor(() => screen.getByTestId("textUpload"));
-});
+beforeEach(() => {
+    Cookies.get.mockReturnValue('testToken');
+})
 
 afterEach(() => {
     cleanup(); // Resets the DOM after each test suite
@@ -16,8 +42,350 @@ afterEach(() => {
 });
 
 describe("TextForm", () => {
-    test("Text upload form renders", () => {
+    test("renders container", async () => {
+        render(<TextForm />);
         const textUpload = screen.getByTestId("textUpload");
         expect(textUpload).toBeInTheDocument();
     })
+
+    test("renders different information if editing", async () => {
+        // Mock service
+        DataService.getItemById.mockResolvedValue(mockResponse);
+
+        // Render
+        await act(async () => {
+            render(
+                <MemoryRouter initialEntries={['/TextForm/1']}>
+                    <Routes>
+                        <Route path="/TextForm/:id" element={<TextForm />} />
+                    </Routes>
+                </MemoryRouter>
+            );
+        });
+
+        // Check results
+        const editHeader = screen.getByTestId("editHeader");
+        const editName = screen.getByTestId("editName");
+        expect(editHeader).toBeInTheDocument();
+        expect(editName).toBeInTheDocument();
+    });
+
+    test("handles error in retrieveMenuItem", async () => {
+        // Mock services
+        const mockedError = new Error("Mocked error");
+        DataService.getItemById.mockRejectedValue(mockedError);
+        const consoleError = jest.spyOn(console, "error").mockImplementation(() => { });
+
+        // Perform test
+        await act(async () => {
+            render(
+                <MemoryRouter initialEntries={['/TextForm/1']}>
+                    <Routes>
+                        <Route path="/TextForm/:id" element={<TextForm />} />
+                    </Routes>
+                </MemoryRouter>
+            );
+        });
+
+        // Check results
+        expect(consoleError).toHaveBeenCalledWith(mockedError);
+    })
+
+    test("can add a new item", async () => {
+        // Mock services
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.createItem.mockResolvedValue({});
+
+        // Setup
+        render(<TextForm />);
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formPrice = screen.getByTestId("formPrice");
+        const consoleLog = jest.spyOn(console, "log").mockImplementation(() => { });
+
+        // Perform test
+        await act(async () => {
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.change(formPrice, { target: { value: '999' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(consoleLog).toHaveBeenCalledWith('Item uploaded successfully');
+    });
+
+    test("can add a new item with multiple prices", async () => {
+        // Setup
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.createItem.mockResolvedValue({});
+        const consoleLog = jest.spyOn(console, "log");
+
+        // Perform test
+        render(<TextForm />);
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formMultiplePrices = screen.getByTestId("formMultiplePrices");
+        await act(async () => { // Toggle multiple prices and wait for rerender
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.click(formMultiplePrices);
+        })
+
+        const formSmallPrice = screen.getByTestId("formSmallPrice");
+        const formLargePrice = screen.getByTestId("formLargePrice");
+        await act(async () => { // Set prices
+            fireEvent.change(formSmallPrice, { target: { value: '999' } });
+            fireEvent.change(formLargePrice, { target: { value: '1499' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(consoleLog).toHaveBeenCalledWith('Item uploaded successfully');
+    });
+
+    test("can update an existing item", async () => {
+        // Mock services
+        DataService.getItemById.mockResolvedValue(mockResponse);
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.updateItem.mockResolvedValue({});
+
+        // Initial render
+        await act(async () => {
+            render(
+                <MemoryRouter initialEntries={['/TextForm/1']}>
+                    <Routes>
+                        <Route path="/TextForm/:id" element={<TextForm />} />
+                    </Routes>
+                </MemoryRouter>
+            );
+        });
+
+        // Setup
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formPrice = screen.getByTestId("formPrice");
+        const consoleLog = jest.spyOn(console, "log").mockImplementation(() => { });
+
+        // Perform test
+        await act(async () => { // Set form fields
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.change(formPrice, { target: { value: '999' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(consoleLog).toHaveBeenCalledWith('Item updated successfully');
+    })
+
+    test("can update an existing item with multiple prices", async () => {
+        // Setup
+        DataService.getItemById.mockResolvedValue(mockResponseMultiplePrices);
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.updateItem.mockResolvedValue({});
+        const consoleLog = jest.spyOn(console, "log");
+
+        // Perform test
+        render(<TextForm />);
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formMultiplePrices = screen.getByTestId("formMultiplePrices");
+        await act(async () => { // Toggle multiple prices and wait for rerender
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.click(formMultiplePrices);
+        })
+        
+        const formSmallPrice = screen.getByTestId("formSmallPrice");
+        const formLargePrice = screen.getByTestId("formLargePrice");
+        await act(async () => { // Set prices
+            fireEvent.change(formSmallPrice, { target: { value: '999' } });
+            fireEvent.change(formLargePrice, { target: { value: '1499' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(consoleLog).toHaveBeenCalledWith('Item uploaded successfully');
+    });
+
+    test("handles error from DataService when uploading an item", async () => {
+        // Mock services
+        const mockedError = new Error("Mocked error");
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.createItem.mockRejectedValue(mockedError);
+
+        // Setup
+        render(<TextForm />);
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formPrice = screen.getByTestId("formPrice");
+        const consoleError = jest.spyOn(console, "error").mockImplementation(() => { });
+
+        // Perform test
+        await act(async () => { // Set form fields
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.change(formPrice, { target: { value: '999' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(consoleError).toHaveBeenCalledWith('Error uploading/updating item:', mockedError);
+    })
+
+    test("alerts the user if they are not logged in", async () => {
+        // Setup
+        Cookies.get.mockReturnValue(undefined); // No token
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        await act(async () => {
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('You need to be logged in to submit the form.');
+    })
+
+    test("alerts the user if they do not enter an item name", async () => {
+        // Setup
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        await act(async () => {
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('Please fill in the name before submitting.');
+    })
+
+    test("alerts the user if they do not enter an item price", async () => {
+        // Setup
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        await act(async () => {
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('Please fill in the price before submitting.');
+    })
+
+    test("alerts the user if they do not enter an item price when the item has multiple prices", async () => {
+        // Setup
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formMultiplePrices = screen.getByTestId("formMultiplePrices");
+        await act(async () => {
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.click(formMultiplePrices);
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('Please fill in both price fields before submitting.');
+    })
+
+    test("alerts the user if the price is not a number", async () => {
+        // Setup
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formPrice = screen.getByTestId("formPrice");
+        await act(async () => {
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.change(formPrice, { target: { value: 'avc' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('Please enter a valid price.');
+    })
+
+    test("alerts the user if one of the prices is not a number when there are multiple prices", async () => {
+        // Setup
+        render(<TextForm />);
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formMultiplePrices = screen.getByTestId("formMultiplePrices");
+        await act(async () => { // Toggle multiple prices and wait for rerender
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.click(formMultiplePrices);
+        })
+
+        const formSmallPrice = screen.getByTestId("formSmallPrice");
+        const formLargePrice = screen.getByTestId("formLargePrice");
+        await act(async () => {
+            fireEvent.change(formSmallPrice, { target: { value: '999' } });
+            fireEvent.change(formLargePrice, { target: { value: 'testError' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('Please enter valid prices for both small and large sizes.');
+    })
+
+    test("alerts the user if they attempt to change the price structure of an existing item", async () => {
+        // Mock services
+        DataService.getItemById.mockResolvedValue(mockResponse);
+        UserService.getUserbyToken.mockResolvedValue('testUser');
+        DataService.updateItem.mockResolvedValue({});
+        const alertSpy = jest.spyOn(window, "alert");
+        alertSpy.mockReturnValue(true);
+
+        // Initial render
+        await act(async () => {
+            render(
+                <MemoryRouter initialEntries={['/TextForm/1']}>
+                    <Routes>
+                        <Route path="/TextForm/:id" element={<TextForm />} />
+                    </Routes>
+                </MemoryRouter>
+            );
+        });
+
+        // Perform test
+        const form = screen.getByTestId("form");
+        const formName = screen.getByTestId("formName");
+        const formMultiplePrices = screen.getByTestId("formMultiplePrices");
+        await act(async () => { // Toggle multiple prices and wait for rerender
+            fireEvent.change(formName, { target: { value: 'testName' } });
+            fireEvent.click(formMultiplePrices);
+        })
+
+        const formSmallPrice = screen.getByTestId("formSmallPrice");
+        const formLargePrice = screen.getByTestId("formLargePrice");
+        await act(async () => {
+            fireEvent.change(formSmallPrice, { target: { value: '1299' } });
+            fireEvent.change(formLargePrice, { target: { value: '1499' } });
+            fireEvent.submit(form);
+        });
+
+        // Check results
+        expect(alertSpy).toHaveBeenCalledWith('You cannot change the price structure for an existing item. '
+            + 'If you want a single price item to have two prices, or a two price item to have one price, then '
+            + 'please create a new item with the right price structure and delete this one.');
+    });
 })
--- my-frontend/src/pages/uploadTextTemplate.js
+++ my-frontend/src/pages/uploadTextTemplate.js
@@ -21,34 +21,26 @@ function TextForm() {
 
   let [menuItem, setMenuItem] = useState();
   let params = useParams();
-  
+
   // eslint-disable-next-line
   useEffect(() => {
-      retrieveMenuItem();
+    retrieveMenuItem();
   }, [params.id]);
-  
+
   const retrieveMenuItem = () => {
     if (params.id) {
-      let url = process.env.REACT_APP_API_BASE_URL+`/items?_id=${params.id}`;
-      fetch(url)
-        .then((response) => {
-          return response.json();
+      DataService.getItemById(params.id)
+      .then((res) => {
+          setMenuItem(res.data[0]);
+          if (res.data[0].price_small && res.data[0].price_large) {
+            setHasMultiplePrices(true);
+          } else {
+            setHasMultiplePrices(false);
+          }
         })
-        .then((data) => {
-        setMenuItem(data[0]);
-        console.log(menuItem);
-
-        if (data[0].price_small !== undefined && data[0].price_large !== undefined) {
-          setHasMultiplePrices(true);
-        } else {
-          setHasMultiplePrices(false);
-        }
-
-        })
-        .catch((e) => 
-        {
-          console.log(e);
-    });
+      .catch((e) => {
+        console.error(e);
+      });
     }
   };
 
@@ -66,10 +58,10 @@ function TextForm() {
 
     if (name === '') {
       // Display an error message or alert the user
-      alert('Please fill in all the name before submitting.');
+      alert('Please fill in the name before submitting.');
       return; // Exit the function without further processing
     }
-    
+
     if (price === '' && !hasMultiplePrices) {
       alert('Please fill in the price before submitting.');
       return;
@@ -78,28 +70,27 @@ function TextForm() {
     if ((priceSmall === '' || priceLarge === '') && hasMultiplePrices) {
       alert('Please fill in both price fields before submitting.');
       return;
-    }    
+    }
 
     //API to my knowledge doesn't handle removing the old price lines, so it might 
     //just be easier to make a new item if the user wants to go from 1 price to 2 prices
-    if ((!hasMultiplePrices && menuItem?.price_small && menuItem?.price_large) ||
-        (hasMultiplePrices && !menuItem?.price_small && !menuItem?.price_large)) {
+    if ((!hasMultiplePrices && (menuItem?.price_small || menuItem?.price_large)) || // Single price but small or large exist
+      (hasMultiplePrices && menuItem?.price)) { // Multiple prices but normal price exists
       alert('You cannot change the price structure for an existing item. If you want a single price item to have two prices, or a two price item to have one price, then please create a new item with the right price structure and delete this one.');
       return;
     }
 
-    
-  // Check if price is not a number when there's a single price
-  if (!hasMultiplePrices && isNaN(price)) {
-    alert('Please enter a valid price.');
-    return;
-  }
+    // Check if price is not a number when there's a single price
+    if (!hasMultiplePrices && isNaN(price)) {
+      alert('Please enter a valid price.');
+      return;
+    }
 
-  // Check if both prices are numbers when there are multiple prices
-  if (hasMultiplePrices && (isNaN(priceSmall) || isNaN(priceLarge))) {
-    alert('Please enter valid prices for both small and large sizes.');
-    return;
-  }
+    // Check if both prices are numbers when there are multiple prices
+    if (hasMultiplePrices && (isNaN(priceSmall) || isNaN(priceLarge))) {
+      alert('Please enter valid prices for both small and large sizes.');
+      return;
+    }
 
     //Create a FormData object to append form fields and file
     const formData = new FormData();
@@ -118,33 +109,33 @@ function TextForm() {
     formData.append('token', token);
 
 
-  // Send the formData to your server for processing
-  try {
-    
-    for (const value of formData.values()) {  //logging for testing
-      console.log(value);
-    }
-    
-    if (menuItem) {
-      // if editing
-      formData.append('currName', menuItem.name);        //only append these if editings
-      formData.append('currCat', menuItem.itemCategory); //api uses them to find item in database
+    // Send the formData to your server for processing
+    try {
 
-      // eslint-disable-next-line
-      const res = await DataService.updateItem(formData);
-      console.log('Item updated successfully'); // Handle success for update
-      
-    } else {
-      // If creating
-      // eslint-disable-next-line
-      const res = await DataService.createItem(formData);
-      console.log('Item uploaded successfully'); // Handle success for upload
-      
+      // for (const value of formData.values()) {  //logging for testing
+      //   console.log(value);
+      // }
+
+      if (menuItem) {
+        // if editing
+        formData.append('currName', menuItem.name);        //only append these if editings
+        formData.append('currCat', menuItem.itemCategory); //api uses them to find item in database
+
+        // eslint-disable-next-line
+        const res = await DataService.updateItem(formData);
+        console.log('Item updated successfully'); // Handle success for update
+
+      } else {
+        // If creating
+        // eslint-disable-next-line
+        const res = await DataService.createItem(formData);
+        console.log('Item uploaded successfully'); // Handle success for upload
+
+      }
+    } catch (error) {
+      // Handle the error (e.g., show an error message to the user)
+      console.error('Error uploading/updating item:', error);
     }
-  } catch (error) {
-    // Handle the error (e.g., show an error message to the user)
-    console.error('Error uploading/updating item:', error);    
-  }
 
     // Reset form fields
 
@@ -152,29 +143,29 @@ function TextForm() {
     setPrice('');
     setPriceSmall('');
     setPriceLarge('');
-	
+
   };
 
   return (
-    
+
     <div data-testid="textUpload">
-		  <div> <br></br><br></br><br></br></div>
+      <div> <br></br><br></br><br></br></div>
       <div className="centered-text">
-          {menuItem && menuItem.name ? <h1>Currently editing "{menuItem.name}"</h1> : <h1>Adding a new menu item</h1>} 
+        {menuItem && menuItem.name ? <h1 data-testid="editHeader">Currently editing "{menuItem.name}"</h1> : <h1>Adding a new menu item</h1>}
       </div>
       <div> <br></br><br></br><br></br></div>
 
-      <form onSubmit={handleSubmit} className="vertical-center">
-        {menuItem && menuItem.name ? <h3>Current item name: {menuItem.name}</h3> : <h3>Name</h3>}
-        <input
+      <form onSubmit={handleSubmit} className="vertical-center" data-testid="form">
+        {menuItem && menuItem.name ? <h3 data-testid="editName">Current item name: {menuItem.name}</h3> : <h3>Name</h3>}
+        <input data-testid="formName"
           type="text"
           placeholder={menuItem && menuItem.name ? "New Item Name" : "Item Name"}
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <br></br>
-      
-          <h3>Category:</h3>
+
+        <h3>Category:</h3>
         <h3>This item is a Lunch & Dinner menu item</h3>
         <br></br>
 
@@ -187,7 +178,7 @@ function TextForm() {
             : 'Price'}
         </h3>
         {!hasMultiplePrices ? (
-          <input
+          <input data-testid="formPrice"
             type="text"
             placeholder="Enter Price"
             value={price}
@@ -195,13 +186,13 @@ function TextForm() {
           />
         ) : (
           <>
-            <input
+            <input data-testid="formSmallPrice"
               type="text"
               placeholder="Small Price"
               value={priceSmall}
               onChange={(e) => setPriceSmall(e.target.value)}
             />
-            <input
+            <input data-testid="formLargePrice"
               type="text"
               placeholder="Large Price"
               value={priceLarge}
@@ -211,8 +202,8 @@ function TextForm() {
         )}
         <br />
         <div className="checkbox-container">
-          <input
-            type="checkbox"
+          <input data-testid="formMultiplePrices"
+            type="checkbox" 
             id="multiplePricesCheckbox"
             checked={hasMultiplePrices}
             onChange={(e) => setHasMultiplePrices(e.target.checked)}
